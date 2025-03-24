7a129e6980f4bf8c9181d091d8e870aad25b1e27
keyboard2005 <samuraizd@outlook.com>
2025-03-24 21:37:21 +0800

    Site updated: 2025-03-24 21:37:21

--- static/selfcode/index.js
+++ static/selfcode/index.js
@@ -1,26 +1,39 @@
-// 目标元素
-const targetElement = document.querySelector("#body-container");
 
-// 创建一个 MutationObserver 实例
-const observer = new MutationObserver((mutationsList) => {
-    if (targetElement.classList.contains('theme-dark')) {
-        document.querySelectorAll(".shiki").forEach((item) => {
-            item.classList.remove("shiki-theme");
-            item.classList.add("github-dark");
+window.onload = function () {
+    setTimeout(() => {
+        const targetElement = document.querySelector("#body-container");
+        if (targetElement.classList.contains('theme-dark')) {
+            document.querySelectorAll(".shiki").forEach((item) => {
+                item.classList.remove("shiki-light");
+                item.classList.add("shiki-dark");
+            });
+        } else {
+            document.querySelectorAll(".shiki").forEach((item) => {
+                item.classList.remove("shiki-dark");
+                item.classList.add("shiki-light");
+            })
+        }
+        // 创建一个 MutationObserver 实例
+        const observer = new MutationObserver((mutationsList) => {
+            if (targetElement.classList.contains('theme-dark')) {
+                console.log(document.querySelectorAll(".shiki"));
+                document.querySelectorAll(".shiki").forEach((item) => {
+                    item.classList.remove("shiki-light");
+                    item.classList.add("shiki-dark");
+                });
+            } else {
+                document.querySelectorAll(".shiki").forEach((item) => {
+                    item.classList.remove("shiki-dark");
+                    item.classList.add("shiki-light");
+                });
+            }
         });
-    } else{
-        document.querySelectorAll(".shiki").forEach((item) => {
-            item.classList.remove("github-dark");
-            item.classList.add("github-light");
-        });
-    }
-});
 
-// 配置选项：监听属性变化
-const config = {
-    attributes: true, // 监听属性变化
-    attributeFilter: ['class'], // 只监听 class 属性
-};
-
-// 开始观察目标元素
-observer.observe(targetElement, config);
+        // 配置选项：监听属性变化
+        const config = {
+            attributes: true, // 监听属性变化
+            attributeFilter: ['class'], // 只监听 class 属性
+        };
+        observer.observe(targetElement, config);
+    }, 500);
+}
